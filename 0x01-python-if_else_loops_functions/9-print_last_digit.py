e36c6bda868b3bb1302e3c6de556887e6250db2e
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-01-03 23:10:45 +0100

    Optimization skills

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,4 +3,6 @@ def print_last_digit(number):
     last_digit = str(number)[len(str(number)) - 1]
     if last_digit.isdigit():
         print(f"{last_digit}", end="")
+    else:
+        sys.stderr("Not a digit")
     return (last_digit)

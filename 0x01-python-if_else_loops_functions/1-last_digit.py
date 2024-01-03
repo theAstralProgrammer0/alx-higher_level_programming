0c630d97461591fa94f03239da6d682505ebdb97
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-01-03 19:50:56 +0100

    Made script pycodestyle compliant again

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -10,5 +10,5 @@ if last_digit > 5:
 elif last_digit == 0:
     print(f"Last digit of {number} is {last_digit} and is 0")
 elif 6 > last_digit != 0:
-    print(f"Last digit of {number} is {last_digit}")
-    print("and is less than 6 and not 0")
+    print(f"Last digit of {number} is {last_digit}", end="")
+    print(" and is less than 6 and not 0")

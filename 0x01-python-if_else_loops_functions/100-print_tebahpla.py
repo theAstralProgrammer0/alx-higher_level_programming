e9d1f7b4ffce1076fa55787d0f6e2a85a75ccf2c
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-01-04 01:21:35 +0100

    Made cipher script pycodestyle compliant

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 for ch_a in range(122, 96, -1):
     # if ascii value is odd
-    print(chr(ch_a) if not ch_a % 2 else chr(ch_a - 32), end="")
+    print("{}".format(chr(ch_a))
+          if not ch_a % 2
+          else "{}".format(chr(ch_a - 32)), end="")

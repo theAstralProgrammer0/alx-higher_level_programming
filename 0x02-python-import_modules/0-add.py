fe6922d324b51d779990058529a845ecb2b95f22
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-01-04 13:46:57 +0100

    Debugging Debbugger

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import add_0 as add_mod
+from add_0 import add
 a = 1
 b = 2
-print("{} + {} = {}".format(a, b, add_mod.add(a, b)))
+print("{} + {} = {}".format(a, b, add(a, b)))

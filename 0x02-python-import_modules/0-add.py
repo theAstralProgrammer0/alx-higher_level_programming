0f5a3c1a00a9fceae82eace5cae763b50ea55ad2
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-01-04 13:37:10 +0100

    Adjusted 0-add.py script to fit requirements of project

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,16 +1,5 @@
 #!/usr/bin/python3
 import add_0 as add_mod
-
-# Wrapper function to intercept arguments
-def wrapper_add(a, b):
-    add_mod.wrap_a = a
-    add_mod.wrap_b = b
-    res_fun = add_mod.add(a, b)
-    return res_fun
-
-result = wrapper_add(1, 2)
-
-a_val = add_mod.wrap_a
-b_val = add_mod.wrap_b
-
-print("{} + {} = {}".format(a_val, b_val, result))
+a = 1
+b = 2
+print("{} + {} = {}".format(a, b, add_mod.add(a, b)))

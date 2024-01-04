97d3923a600ad9a1253ad7cc420a2df0e3e32e96
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-01-04 02:59:59 +0100

    Added a C algorithm that inserts a new node into a sorted linked list

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * insert_node - Entry Point
+ *
+ * Description: This function inserts a node at the correct position in a
+ * sorted linked list
+ *
+ * @head: double pointer to head of linked list
+ *
+ * @number: Number member of the new node
+ *
+ * Return: (newnode) pointer to the newly inserted node
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *current = NULL;
+	listint_t *nextnode = NULL;
+	listint_t *newnode = NULL;
+
+	if (head == NULL || *head == NULL)
+		return (NULL);
+
+	current = *head;
+
+	newnode = malloc(sizeof(listint_t));
+	if (newnode == NULL)
+		return (NULL);
+	newnode->n = number;
+
+	while (current->n < number && current->next->n < number)
+		current = current->next;
+
+	nextnode = current->next;
+	current->next = newnode;
+	newnode->next = nextnode;
+	return (newnode);
+}
+

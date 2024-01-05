43d3a12264394410f6477b83ea59d2a5569b36ee
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-01-05 02:00:49 +0100

    Made 100-my_calculator.py pycodestyle compliant

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,14 +2,18 @@
 import sys
 from calculator_1 import add, sub, mul, div
 
+
 def main():
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
     a, op, b = sys.argv[1:4]
-    ops = {"+" : add, "-" : sub, "*" : mul, "/" : div}
-    
+    ops = {"+": add,
+           "-": sub,
+           "*": mul,
+           "/": div
+           }
     if op in ops:
         result = ops[sys.argv[2]](int(a), int(b))
         print(f"{a} {op} {b} = {result}")

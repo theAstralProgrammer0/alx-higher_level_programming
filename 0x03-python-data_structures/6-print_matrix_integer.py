6807957c86ff2c9468978a8703fdae2e7f823b30
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-01-07 10:51:35 +0100

    Requirement check

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,4 +3,4 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for col in row:
             print("{} ".format(col), end="")
-        print("{}".format('\n'))
+        print("{}".format('\n'), end="")
---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    #(1, 3) + (9, 6) = (10, 9)
+    result = ()
+    if len(tuple_a) == 0:
+        t_a = tuple_a + (0, 0)
+    elif len(tuple_a) == 1:
+        t_a_p = list(tuple_a)
+        t_a_p.append(0)
+        t_a = tuple(t_a_p_l)
+    else:
+        t_a = tuple_a
+
+    if len(tuple_b) == 1:
+        t_b_p = list(tuple_b)
+        t_b_p.append(0)
+        t_b = tuple(t_b_p)
+    elif len(tuple_b) == 0:
+        t_b = tuple_b + (0, 0)
+    else:
+        t_b = tuple_b
+
+    for i, j in range(2), range(2):
+        print(t_a[0])
+        print(t_b[1])
+        return result + tuple([t_a[i] + t_b[i]]) + tuple([t_a[j] + t_b[j]])

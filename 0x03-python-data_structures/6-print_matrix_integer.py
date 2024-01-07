3a2f3ab857436743082cd08e22bf261dfe6044db
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-01-07 10:49:57 +0100

    Requirement check

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,4 +3,4 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for col in row:
             print("{} ".format(col), end="")
-        print()
+        print("{}".format('\n'))

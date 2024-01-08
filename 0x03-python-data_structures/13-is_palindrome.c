f9ab9ac753a383bbf995d0f7d017f7b9a495e4bc
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-01-08 16:27:56 +0100

    Optimized using recursions

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,43 +1,26 @@
 #include "lists.h"
-listint_t* tte(listint_t* tail)
+void tte(listint_t* temp, listint_t* tail, int* ret)
 {
 	if (tail->next == NULL)
-		return (tail);
-	return (tte(tail->next));
-}
-
-listint_t* cttp(listint_t* current, listint_t* tail)
-{
-	if (current->next == tail)
-		return (current);
-	return (cttp(current->next, tail));
-}
-
-int pali_recur(listint_t* current, listint_t* temp, listint_t* tail)
-{
-	if (temp == tail || tail->next == temp)
-		return (1);
+		return;
+	tte(temp->next, tail->next, ret);
 	if (temp->n == tail->n)
 	{
-		current = temp;
-		temp = temp->next;
-		tail = cttp(current, tail);
-		return (pali_recur(current, temp, tail));
+		*ret = 1;
 	}
-	return (0);
 }
+
 
 int is_palindrome(listint_t **head)
 {
 	listint_t *temp = NULL;
 	listint_t *tail = NULL;
 	listint_t *current = NULL;
-	int ret;
+	int ret = 0; 
 
 	if (head == NULL || *head == NULL)
 		return (1);
 	temp = current = tail = *head;
-	tail = tte(tail);
-	ret = pali_recur(current, temp, tail);
+	tte(temp, tail, &ret);
 	return (ret);
 }

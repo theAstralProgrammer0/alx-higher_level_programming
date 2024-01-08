083d155086cca56b28656106f68e23feb59bc6e1
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-01-08 17:05:13 +0100

    Debugging Debugger

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,12 +1,12 @@
 #include "lists.h"
-void tte(listint_t* temp, listint_t* tail, int* ret)
+void tte(listint_t** temp, listint_t* tail, int* ret)
 {
 	if (tail->next == NULL)
 		return;
 	tte(temp, tail->next, ret);
-	if (temp->n == tail->n)
+	if ((*temp)->n == tail->n)
 	{
-		temp = temp->next;
+		*temp = (*temp)->next;
 		*ret = 1;
 	}
 }
@@ -14,13 +14,14 @@ void tte(listint_t* temp, listint_t* tail, int* ret)
 
 int is_palindrome(listint_t **head)
 {
-	listint_t *temp = NULL;
+	listint_t **temp = NULL;
 	listint_t *tail = NULL;
 	int ret = 0; 
 
 	if (head == NULL || *head == NULL)
 		return (1);
-	temp = tail = *head;
+	tail = *head;
+	temp = head;
 	tte(temp, tail, &ret);
 	return (ret);
 }

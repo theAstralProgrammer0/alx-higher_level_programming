13b5be63fa78664a85ba2eb87cb83a924e95afc8
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-01-10 12:46:14 +0100

    Made 4-only...py pycodestyle compliant

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    return (set_1 - set_1.intersection(set_2)).union(set_2 - set_2.intersection(set_1))
+    new_set_1 = set_1 - set_1.intersection(set_2)
+    new_set_2 = set_2 - set_2.intersection(set_1)
+    return new_set_1.union(new_set_2)

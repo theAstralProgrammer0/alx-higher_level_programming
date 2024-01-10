8bc20f91d6da7f5b31c33b34377b6ff29158e922
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-01-10 19:07:56 +0100

    Made script pycodestyle complaint

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,5 +2,5 @@
 def multiply_by_2(a_dictionary):
     new_dict = a_dictionary.copy()
     for key in list(new_dict.keys()):
-        new_dict.update({key : new_dict[key]*2})
+        new_dict.update({key: new_dict[key]*2})
     return new_dict

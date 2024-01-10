f2ec52344b4b46874f917be786e5c04fb01cbd58
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-01-10 22:28:04 +0100

    Made file pycodestyle compliant

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-my_list = [(1, 2), (2, 1), (3, 10), (4, 2)]
 def weight_average(my_list=[]):
     if not my_list:
         return 0
@@ -9,5 +8,3 @@ def weight_average(my_list=[]):
         num += (tup[0] * tup[1])
         den += tup[1]
     return num / den
-
-

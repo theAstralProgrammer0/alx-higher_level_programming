44249e2611a83955e5c63e1e60483eb8fcc854e7
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-01-10 20:42:46 +0100

    Debugging Debugger

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if not a_dictionary:
         return None
-    return max(a_dictionary, key=a_dictionary.get)
+    best_key = None
+    best_value = None
+
+    for key, value in a_dictionary.items():
+        if best_value is None or value > best_value:
+            best_key = key
+            best_value = value
+    return best_key

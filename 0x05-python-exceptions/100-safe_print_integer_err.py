dbc02ac486467b196663608171f2b0e359fb8db6
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-01-22 14:01:00 +0100

    Added new script

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+def safe_print_integer_err(value):
+    printed = True
+
+    try:
+        print("{:d}".format(value))
+    except (TypeError, ValueError) as err_msg:
+        print("Exception: {}".format(err_msg), file=sys.stderr)
+        return not printed
+    else:
+        return printed

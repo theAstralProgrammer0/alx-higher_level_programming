886264a8675c8409d427150224938e5904f0c50c
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-01-22 09:16:37 +0100

    Added start file

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    for i in range(x):
+        try:
+            print(f"{my_list[i]}", end="")
+        except IndexError:
+            pass
+        else:
+            count += 1
+            i += 1
+    print('\n')
+    return (count)

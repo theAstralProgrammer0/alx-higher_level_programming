8a26418f379b355ac98db40c2dc4938daa9a2a9c
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-01-22 09:47:48 +0100

    Added safe print int

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    printed = False
+
+    try:
+        value = int(value)
+    except ValueError:
+        pass
+    else:
+        print("{:d}".format(value))
+        printed = not printed
+    finally:
+        return (printed)

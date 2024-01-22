290e11ee65346a0e872685d1b6f0f435a93e1d9c
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-01-22 09:56:13 +0100

    Added safe print int

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -7,7 +7,7 @@ def safe_print_integer(value):
     except (TypeError, ValueError):
         pass
     else:
-        print("{:d}".format(value))
+        print("{:d}\n".format(value), end="")
         printed = not printed
     finally:
         return (printed)

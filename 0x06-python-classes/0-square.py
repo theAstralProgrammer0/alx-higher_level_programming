6e6b28ba8f73881d9c769fb52f29b3a7ce7ed60f
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-01-23 07:16:14 +0100

    Added new class

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+    This is the documentation for this module
+"""
+class Square:
+    """
+        This is the documentation for the square class
+    """
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+THis is the README fiel for classes in python

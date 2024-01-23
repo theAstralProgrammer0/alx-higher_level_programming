e2b15f4d00385b743c377f93f19d8e55b47e9df9
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-01-23 19:48:49 +0100

    Added script:

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import dis
+
+
+class MagicClass:
+    def __init__(self, radius=0):
+        #self.__radius = 0
+
+        if type(radius) is not int and type(radius) is not float:
+                raise TypeError("radius must be a number")
+        self.radius = radius
+    
+    def area(self):
+        return ((self.__radius ** 2) * math.pi)
+
+    def circumference(self):
+        return ((math.pi * 2) * self.__radius)
+
+
+dis.dis(MagicClass.__init__)

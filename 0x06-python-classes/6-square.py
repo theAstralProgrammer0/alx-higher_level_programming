4d99939ef1099ad3949365c0972cf00a7037c3b8
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-01-23 11:17:28 +0100

    Debugging Debugger

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -46,10 +46,10 @@ class Square:
         return self.__position
 
     @position.setter
-    def position(self, tup):
-        if not isinstance(tup, tuple) or len(tup) != 2:
+    def position(self, value):
+        if not isinstance(value, tuple) or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = tup
+        self.__position = value
 
     def area(self):
         """

8aad32aef9f7657326bc2dce99cf85eab95a72bf
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-01-23 09:43:57 +0100

    Pycodestyle compliance

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -25,7 +25,7 @@ class Square:
                 size (int): The size of the square
         """
         if not isinstance(size, int):
-            raise TypeError("size is not an integer")
+            raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size

df14b53712d7bb28f6749384a5145019f06c8e79
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-01-24 01:20:07 +0100

    Added disassembled python bytecode

--- 0x05-python-exceptions/0-main.py
+++ 0x05-python-exceptions/0-main.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+import dis
+
+
 safe_print_list = __import__('0-safe_print_list').safe_print_list
 
 my_list = [1, 2, 3, 4, 5]
@@ -9,4 +12,4 @@ nb_print = safe_print_list(my_list, len(my_list))
 print("nb_print: {:d}".format(nb_print))
 nb_print = safe_print_list(my_list, len(my_list) + 2)
 print("nb_print: {:d}".format(nb_print))
-
+dis.dis(safe_print_list)

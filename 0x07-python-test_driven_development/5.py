6beb0f2d7600d3916e96e341ea7e71a9bca20152
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-02-01 11:45:37 +0100

    Correction

---
+++ 0x07-python-test_driven_development/5.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+text_indentation module:
+This module provides a function to indent text after specific punctuation
+marks.
+"""
+
+
+def text_indentation(text):
+    """
+    Indents text after periods, question marks, and colons.
+    Prints the indented text
+
+    Args:
+        text: The text to be indented.
+
+    Returns:
+        Nothing
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    new_text = ""
+    leadingSpace = True
+    ender = ""
+
+    for i in range(len(text)):
+        if (i == len(text) - 1 and
+            (text[i] != '.' and 
+             text[i] != ':' and
+             text[i] != '?' and
+             text[i] != ' ')):
+            ender = '\n'
+        if text[i] == '\n':
+            new_text += text[i]
+            leadingSpace = True
+            continue
+        if text[i] == ' ':
+            if i == len(text) - 1:
+                break
+            elif text[i + 1] == '\n':
+                i += 1
+                continue
+            elif leadingSpace:
+                continue
+        leadingSpace = False
+        if text[i] != ':' and text[i] != '.' and text[i] != '?':
+            new_text += text[i]
+            continue
+        else:
+            new_text += text[i] + "\n\n"
+            leadingSpace = True
+    print(new_text, end=ender)

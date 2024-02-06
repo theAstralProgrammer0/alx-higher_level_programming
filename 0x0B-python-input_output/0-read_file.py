c9df6ba9ccde413318b947a11a0f163cd2a6e8b0
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-02-06 12:39:34 +0100

    First module

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 """This module reads the content of a file named @filename"""
+
+
 def read_file(filename=""):
+    """This function perfroms the module's function"""
     with open("my_file_0.txt", mode="r", encoding="utf-8") as myFile:
         print(myFile.read())

d36dc4dc8bb22f0c2b1f46d8c40148b992171da6
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-02-06 19:24:33 +0100

    Twelveth Module/Function

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""This module contains a Student class"""
+
+
+class Student:
+    __slot__ = []
+    """This is a Student class"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """This is a function that retrieves the json (dict) form of an obj"""
+        i_dct = self.__dict__
+        if attrs is None:
+            return i_dct
+        return {key: value for key, value in i_dct.items() if key in attrs}
+
+    def reload_from_json(self, json):
+        """This is a function that replaces all attributes of the Student
+           instance
+        """
+        dic = self.__dict__
+        return {key: json[key] for key, value in dic.items()}

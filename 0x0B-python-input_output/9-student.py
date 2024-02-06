272f0a1e138108598c6d6dcef197233f4807c2b4
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-02-06 18:27:20 +0100

    Tenth Function

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,12 +1,14 @@
-#!?usr/bin/python3
+#!/usr/bin/python3
 """This module contains a Student class"""
 
 
 class Student:
+    """This is a Student class"""
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
+        """This is a function that retrieves the json (dict) form of an obj"""
         return self.__dict__

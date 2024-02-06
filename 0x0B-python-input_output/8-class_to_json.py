3d789c17d759a47c14dad27bf45709be2ebf185c
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-02-06 18:15:32 +0100

    Nineth Function

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module contains a function that returns the dictionary description with
+   simple data structure (list, dictionary, string, integer and boolean) for
+   JSON serialization of an object:
+"""
+
+
+def class_to_json(obj):
+    """This is a function that returns the dictionary description with simple
+       data structure (list, dictionary, string, integer and boolean) for JSON
+       serialization of an object:
+    """
+    return obj.__dict__ 

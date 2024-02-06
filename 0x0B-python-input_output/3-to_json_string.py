37c702d83c80c95be40f6885b2c8dfe03af80e36
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-02-06 13:23:30 +0100

    Fourth module

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -6,4 +6,7 @@ import json
 
 
 def to_json_string(my_obj):
+    """This is a function that returns the JSON representation of an
+       object (string)
+    """
     return json.dumps(my_obj, ensure_ascii=False)

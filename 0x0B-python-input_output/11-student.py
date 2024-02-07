943a6bd1e1859e374629bd971616f555961e638b
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-02-07 05:50:21 +0100

    Student

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,25 +1,41 @@
 #!/usr/bin/python3
-"""This module contains a Student class"""
+"""Defines a class Student."""
 
 
 class Student:
-    __slot__ = []
-    """This is a Student class"""
+    """Represent a student."""
+
     def __init__(self, first_name, last_name, age):
+        """Initialize a new Student.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """This is a function that retrieves the json (dict) form of an obj"""
-        i_dct = self.__dict__
-        if attrs is None:
-            return i_dct
-        return {key: value for key, value in i_dct.items() if key in attrs}
+        """Get a dictionary representation of the Student.
+
+        If attrs is a list of strings, represents only those attributes
+        included in the list.
+
+        Args:
+            attrs (list): (Optional) The attributes to represent.
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
 
     def reload_from_json(self, json):
-        """This is a function that replaces all attributes of the Student
-           instance
+        """Replace all attributes of the Student.
+
+        Args:
+            json (dict): The key/value pairs to replace attributes with.
         """
-        dic = self.__dict__
-        return {key: json[key] for key, value in dic.items()}
+        for k, v in json.items():
+            setattr(self, k, v)

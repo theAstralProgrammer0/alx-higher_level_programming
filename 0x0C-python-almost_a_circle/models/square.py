4d7a53cdf8c674c7440bc13b4af49cc6d2c37fb5
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-02-13 00:47:44 +0100

    Pycodestyle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -2,18 +2,22 @@
 """This is the module that contains the 'Square' class"""
 from models.rectangle import Rectangle
 
+
 class Square(Rectangle):
     """This is the 'Square' class"""
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
         self.size = size
-        
+
     def __str__(self):
+        """This is the str magic method for 'print' statements to work with"""
         return "[Square] ({}) {}/{} - {}".format(self.id,
                                                  self.x,
                                                  self.y,
                                                  self.size)
+
     def update(self, *args, **kwargs):
+        """This method updates the instance attributes of a square object"""
         attr_list = ["id", "size", "x", "y"]
         if not args:
             for key, value in kwargs.items():
@@ -23,6 +27,9 @@ class Square(Rectangle):
                 setattr(self, str(attr_list[i]), arg)
 
     def to_dictionary(self):
+        """This method generates a dict based on the attribute-values of a
+           rectangle instance
+        """
         sqr_dict = dict(id=self.id, x=self.x, size=self.size, y=self.y)
         return sqr_dict
 

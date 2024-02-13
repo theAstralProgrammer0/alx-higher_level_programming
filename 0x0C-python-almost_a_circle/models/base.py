d7f95bc25af63efb42ae61e8ab86229a26ffc8ea
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-02-13 01:31:12 +0100

    Debugging Base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -23,6 +23,8 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries=[]):
         """This is the serialization function"""
+        if not list_dictionaries:
+            return "[]"
         return json.dumps(list_dictionaries)
 
     @staticmethod

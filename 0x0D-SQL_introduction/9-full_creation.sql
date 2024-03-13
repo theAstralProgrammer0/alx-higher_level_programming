95cfb2d7c7c56c18cb3866c7a99b7b36a442eb4b
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-03-13 11:43:54 +0100

    Correction

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -14,11 +14,11 @@
 -- id = 3, name = “Bob”, score = 14
 -- id = 4, name = “George”, score = 8
 CREATE TABLE IF NOT EXISTS second_table(
-	id INT PRIMARY KEY AUTO_INCREMENT,
+	id INT,
 	name VARCHAR(256),
 	score INT
 	);
-INSERT INTO second_table(name, score) VALUES ("John", 10);
-INSERT INTO second_table(name, score) VALUES ("Alex", 3);
-INSERT INTO second_table(name, score) VALUES ("Bob", 14);
-INSERT INTO second_table(name, score) VALUES ("George", 8);
+INSERT INTO second_table(id, name, score) VALUES (1, "John", 10);
+INSERT INTO second_table(id, name, score) VALUES (2, "Alex", 3);
+INSERT INTO second_table(id, name, score) VALUES (3, "Bob", 14);
+INSERT INTO second_table(id, name, score) VALUES (4, "George", 8);

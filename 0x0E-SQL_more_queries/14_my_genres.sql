2a286efa568577cf6329f7072d1444e579001056
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-03-14 10:18:36 +0100

    correction

--- 0x0E-SQL_more_queries/14_my_genres.sql
+++ 0x0E-SQL_more_queries/14_my_genres.sql
@@ -11,4 +11,4 @@ SELECT DISTINCT tv_genres.name
 FROM tv_genres JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_show_genres.show_id = 8
-ORDER BY tv_genres.name
+ORDER BY tv_genres.name ASC

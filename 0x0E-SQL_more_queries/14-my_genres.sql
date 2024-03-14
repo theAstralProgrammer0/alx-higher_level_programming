aabb25d0ad2f7ae57900f0189b0b56137378b6a1
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-03-14 15:19:08 +0100

    Corrected fifteenth script to use two joins

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -8,7 +8,10 @@
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
 SELECT tv_genres.name
-FROM tv_genres JOIN tv_show_genres
+FROM tv_genres
+JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_show_genres.show_id = 8
+JOIN tv_shows
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title = 'Dexter'
 ORDER BY tv_genres.name

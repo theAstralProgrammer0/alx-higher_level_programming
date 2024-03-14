906587d57fd0ac430d9c7844a9c95f9fa55ad071
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-03-14 16:57:09 +0100

    Added negating eighteenth script

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -7,7 +7,17 @@
 -- Results must be sorted in ascending order by the genre name
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
-SELECT DISTINCT tv_genres.name
-FROM tv_genres JOIN tv_shows
-WHERE tv_shows.title = 'Dexter' 
+SELECT tv_genres.name
+FROM tv_genres
+WHERE tv_genres.name NOT IN 
+(
+SELECT tv_genres.name
+FROM tv_genres
+JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name
+)
 ORDER BY tv_genres.name ASC

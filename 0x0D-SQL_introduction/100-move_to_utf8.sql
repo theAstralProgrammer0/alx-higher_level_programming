35339999a534d7acb61efa09b61939b37c4a8d73
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-03-14 12:59:58 +0100

    Correction

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -5,7 +5,6 @@
 -- Database hbtn_0c_0
 -- Table first_table
 -- Field name in first_table
-ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
-ALTER TABLE first_table ROW_FORMAT=DYNAMIC;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table CHANGE name name TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
+ALTER TABLE first_table CHANGE name name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
